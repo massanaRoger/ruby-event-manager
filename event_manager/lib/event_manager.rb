d74b90210935164c6f91722c6bd907474c64e500
Roger Massana <rogermassana2002@gmail.com>
2022-08-10 17:49:57 +0200

    Outputs the hours with the biggest traffic

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,9 +1,23 @@
+# frozen_string_literal: true
+
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def clean_phone_number(number)
+  number = number.to_s
+  number = number.gsub(' ', '')
+  number = number.gsub('-', '')
+  number = number.gsub('(', '')
+  number = number.gsub(')', '')
+  clean_number = ''
+  clean_number = number if number.length == 10
+  clean_number = number[1, -1] if number.chr == 1 && number.length == 11
+  clean_number
 end
 
 def legislators_by_zipcode(zip)
@@ -14,14 +28,14 @@ def legislators_by_zipcode(zip)
     civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -29,6 +43,20 @@ def save_thank_you_letter(id,form_letter)
   File.open(filename, 'w') do |file|
     file.puts form_letter
   end
+end
+
+def best_hours(contents)
+  hash = contents.reduce(Hash.new(0)) do |hash, row|
+    row_arr = row[:regdate].split(" ")
+    #"11/23/08 20:44"
+    #row_date = Date.strptime(row_arr[0], "%m/%d/%y")
+    #p row_date
+    row_time = Time.strptime(row_arr[1], "%H:%M")
+    hash[row_time.hour.to_s] += 1
+    hash
+  end
+  max_hours = hash.values.max
+  hash.select {|k, v| v == max_hours}
 end
 
 puts 'EventManager initialized.'
@@ -42,13 +70,19 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+
+puts "The best hours are #{best_hours(contents).keys.join(', ')}"
+
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  phone = clean_phone_number(row[:homephone])
+  puts row[:homephone]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
-
+  puts "Phone number: #{phone}"
   form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id,form_letter)
+  save_thank_you_letter(id, form_letter)
 end
